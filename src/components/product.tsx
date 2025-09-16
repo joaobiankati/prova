36a742edb53b90b07b56ef6ece9f43827325557f
joao <joaobiankati0164@gmail.com>
2025-09-15 22:37:09 -0300

    Adicionei os estilos finais

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,5 +1,6 @@
 import { Text, TouchableOpacity, View } from "react-native";
-import { styles } from "./styles"
+import { styles } from './styles'
+import { StyleSheet } from "react-native";
 
 type Props = {
   name: string;
@@ -12,8 +13,40 @@ export function Product({ name, onRemove }: Props) {
       <Text style={styles.name}>{name}</Text>
 
       <TouchableOpacity style={styles.button} onPress={onRemove}>
-        <Text style={styles.buttonText}>🗑</Text>
+        <Image>
+          source={require(../../delete.png)}
+        </Image>
       </TouchableOpacity>
     </View>
   );
 }
+
+export const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    backgroundColor: "#262626",
+    borderRadius: 6,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 16,
+    marginBottom: 8,
+  },
+  name: {
+    color: "#fff",
+    fontSize: 16,
+    flex: 1
+  },
+  button: {
+    width: 32,
+    height: 32,
+    backgroundColor: "#E23C44",
+    borderRadius: 6,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  }
+});
